9450340f6d77e92b3a3699cd187d7d867d4057bd
ChemFer <49997528+ChemFer@users.noreply.github.com>
2021-08-01 23:24:32 +0200

    Create Main.java with Reverse Polish Notation calculator
    User input - String

---
+++ Main.java
@@ -0,0 +1,68 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) {
+        int result = polishReverseNotation(getValuesFromUser());
+        System.out.println(result);
+    }
+
+    public static String getValuesFromUser(){
+        System.out.println("Give reverse polish notation: ");
+        Scanner scan = new Scanner(System.in);
+        String rpn = scan.nextLine();
+        return rpn;
+    }
+
+    public static int polishReverseNotation(String tokens){
+        Stack<String> stack = new Stack<String>();
+        String action = "";
+        int x, y;
+        String result = "";
+        int value;
+        String operators = "+-*/";
+
+        for(String a : tokens.split("[^0-9*/+-]")){
+            if(!a.equals("+") && !a.equals("-") && !a.equals("*") && !a.equals("/")){
+                stack.push(a);
+            }
+            else {
+                action = a;
+
+                switch (action){
+                    case "+":
+                        x = Integer.parseInt(stack.pop());
+                        y = Integer.parseInt(stack.pop());
+                        value = x + y;
+                        result = "" + value;
+                        stack.push(result);
+                        break;
+                    case "-":
+                        x = Integer.parseInt(stack.pop());
+                        y = Integer.parseInt(stack.pop());
+                        value = x - y;
+                        result = "" + value;
+                        stack.push(result);
+                        break;
+                    case "*":
+                        x = Integer.parseInt(stack.pop());
+                        y = Integer.parseInt(stack.pop());
+                        value = x * y;
+                        result = "" + value;
+                        stack.push(result);
+                        break;
+                    case "/":
+                        x = Integer.parseInt(stack.pop());
+                        y = Integer.parseInt(stack.pop());
+                        value = y / x;
+                        result = "" + value;
+                        stack.push(result);
+                        break;
+                    default:
+                        continue;
+                }
+            }
+        }
+        return Integer.parseInt(stack.pop());
+    }
+}
